db82896409ad98de98518ccc21cda331e03c7c59
kpondaugusto <64617998+kpondaugusto@users.noreply.github.com>
2021-08-25 10:36:11 -0500

    fixed graph axis

--- cells-chopped-abspeak.py
+++ cells-chopped-abspeak.py
@@ -31,14 +31,14 @@ data_out = pd.concat([df_bake],axis=1)
 
 
 
-data_out.plot.line(style='.-',color={"Cell10bake1": "pink", "Cell10bake2": "blue",
+data_out.plot.line(style='.',color={"Cell10bake1": "pink", "Cell10bake2": "blue",
                                     "Cell10bake3": "pink", 
                                     "Cell12bake1": "blue", 
                                     "Cell13bake1": "blue",
                                     "Cell13bake2": "blue",  
                                     "Cell14bake1": "pink","Cell15bake1": "green",
                                     "Cell16bake1": "pink","Cell17bake1": "green"}, legend=False,
-                   xlabel='Time', ylabel='Lifetime (ms)')
+                   xlabel='Time', ylabel='Absorption Peak')
 
 #blue = short bake (1 - 8 hrs in oven or water bath)
 #pink = long bake (23 + hrs in oven)
@@ -61,7 +61,7 @@ df_nobake = df[keep_col2]
 
 data_out = pd.concat([df_nobake],axis=1)
 
-data_out.plot(style='.',xlabel='Time', ylabel='Lifetime (ms)')
+data_out.plot(style='.',xlabel='Time', ylabel='Absorption Peak')
 
 plt.tick_params(
     axis='x',          # changes apply to the x-axis
@@ -84,7 +84,7 @@ df_onecell = df[keep_col3]
 
 data_out = pd.concat([df_onecell],axis=1)
 
-data_out.plot(style='.',xlabel='Time', ylabel='Lifetime (ms)')
+data_out.plot(style='.',xlabel='Time', ylabel='Absorption Peak')
 
 plt.axvline(3.5,color='r')
 
